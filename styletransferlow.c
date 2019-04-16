6e6c607e5088a43200fc2eec846a95b14a22274e
Pranav Sriram <psriram2@illinois.edu>
2019-04-16 16:01:51 -0500

    added in vgg16 getTensor functions

--- styletransferlow.c
+++ styletransferlow.c
@@ -1,6 +1,165 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+
+
+static float content_tensor1[75][75][256];
+static float content_tensor2[75][75][256];
+static float style_tensor1[19][19][512];
+static float style_tensor2[298][300][64];
+
 // currently everything is a fixed width, I'm hoping the vgg16 function pads for us
 static int h, w, d;
+
+void getContentTensor1(){
+  // Shape of content tensor is 1x75x75x256
+
+
+  // HOW THE TENSOR IS FORMATTED:
+  // Every dimension index is accounted for in the second index so if you want
+  // to access dimension index 2, e.g. content_tensor[0][2][0][0]
+  // The third dimension is each of the 75 lines, and the fourth dimension is the 256 values
+  // in each line
+  
+  char discard[256];
+  float read_num;
+  FILE* content_file;
+  if((content_file = fopen("content.txt","r")) == NULL){
+    printf("Content file not found!");
+    exit(1);
+  }
+
+  // Discard the first three lines of the content file
+  fgets(discard, 50, content_file);
+  printf("%s\n",discard);
+  fgets(discard, 50, content_file);
+  printf("%s\n",discard);
+
+  // Iterate through file and get tensor values
+  for(int i = 0; i < 75; i++){
+    for(int j = 0; j < 75; j++){
+      for(int k = 0; k < 256; k++){
+        fscanf(content_file,"%f",&read_num);
+        content_tensor1[i][j][k] = read_num;
+      }
+    }
+
+  }
+
+
+}
+void getContentTensor2(){
+  // Shape of content tensor is 1x75x75x256
+
+
+  // HOW THE TENSOR IS FORMATTED:
+  // Every dimension index is accounted for in the second index so if you want
+  // to access dimension index 2, e.g. content_tensor[0][2][0][0]
+  // The third dimension is each of the 75 lines, and the fourth dimension is the 256 values
+  // in each line
+  
+  char discard[256];
+  float read_num;
+  FILE* content_file;
+  if((content_file = fopen("content2.txt","r")) == NULL){
+    printf("Content file not found!");
+    exit(1);
+  }
+
+  // Discard the first three lines of the content file
+  fgets(discard, 50, content_file);
+  printf("%s\n",discard);
+  fgets(discard, 50, content_file);
+  printf("%s\n",discard);
+
+  // Iterate through file and get tensor values
+  for(int i = 0; i < 75; i++){
+    for(int j = 0; j < 75; j++){
+      for(int k = 0; k < 256; k++){
+        fscanf(content_file,"%f",&read_num);
+        content_tensor2[i][j][k] = read_num;
+      }
+    }
+
+  }
+
+
+}
+
+void getStyleTensor1(){
+    // Shape of content tensor is 1x19x19x512
+
+  // HOW THE TENSOR IS FORMATTED:
+  // Every dimension index is accounted for in the second index so if you want
+  // to access dimension index 2, e.g. content_tensor[0][2][0][0]
+  // The third dimension is each of the 75 lines, and the fourth dimension is the 256 values
+  // in each line
+  
+  char discard[256];
+  float read_num;
+  FILE* content_file;
+  if((content_file = fopen("style.txt","r")) == NULL){
+    printf("Content file not found!");
+    exit(1);
+  }
+
+  // Discard the first three lines of the content file
+  fgets(discard, 50, content_file);
+  printf("%s\n",discard);
+  fgets(discard, 50, content_file);
+  printf("%s\n",discard);
+
+  // Iterate through file and get tensor values
+  for(int i = 0; i < 19; i++){
+    for(int j = 0; j < 19; j++){
+      for(int k = 0; k < 512; k++){
+        fscanf(content_file,"%f",&read_num);
+        style_tensor1[i][j][k] = read_num;
+      }
+    }
+
+  }
+
+
+}
+
+void getStyleTensor2(){
+    // Shape of content tensor is 1x298x300x64
+
+  // HOW THE TENSOR IS FORMATTED:
+  // Every dimension index is accounted for in the second index so if you want
+  // to access dimension index 2, e.g. content_tensor[0][2][0][0]
+  // The third dimension is each of the 75 lines, and the fourth dimension is the 256 values
+  // in each line
+  
+  char discard[256];
+  float read_num;
+  FILE* content_file;
+  if((content_file = fopen("style2.txt","r")) == NULL){
+    printf("Content file not found!");
+    exit(1);
+  }
+
+  // Discard the first three lines of the content file
+  fgets(discard, 50, content_file);
+  printf("%s\n",discard);
+  fgets(discard, 50, content_file);
+  printf("%s\n",discard);
+
+  // Iterate through file and get tensor values
+  for(int i = 0; i < 298; i++){
+    for(int j = 0; j < 300; j++){
+      for(int k = 0; k < 64; k++){
+        fscanf(content_file,"%f",&read_num);
+        style_tensor2[i][j][k] = read_num;
+      }
+    }
+
+  }
+
+}
+
+
 // mean squared error funciton, which is our loss function
 //
 double mean_squared_error(double a_tensor [h][w][d], double b_tensor [h][w][d], int height, int width, int depth){
